f8b9e1d805395f6cc201568882f7e43591b8a681
Jeroen Knoops <jeroen.knoops@gmail.com>
2018-12-03 17:58:25 +0100

    Chore: Upgrades emotion to version 10

--- workspaces/web-app/pages/_document.js
+++ workspaces/web-app/pages/_document.js
@@ -1,11 +1,9 @@
 import Document, { Head, Main, NextScript } from 'next/document'
-import { extractCritical } from 'emotion-server'
 
 export default class MyDocument extends Document {
   static getInitialProps ({ renderPage }) {
     const page = renderPage()
-    const styles = extractCritical(page.html)
-    return { ...page, ...styles }
+    return { ...page }
   }
 
   constructor (props) {
--- workspaces/web-app/setup-tests.js
+++ workspaces/web-app/setup-tests.js
@@ -2,6 +2,6 @@ import 'jest-dom/extend-expect'
 import 'react-testing-library/cleanup-after-each'
 
 import { createSerializer } from 'jest-emotion'
-import * as emotion from 'emotion'
+import * as emotion from '@emotion/core'
 
 expect.addSnapshotSerializer(createSerializer(emotion))
