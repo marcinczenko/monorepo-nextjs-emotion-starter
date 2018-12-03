25703fc0c68717924127f7b4ae67912f7fda4810
Jeroen Knoops <jeroen.knoops@gmail.com>
2018-12-03 17:58:25 +0100

    Chore: Upgrades emotion to version 10

--- workspaces/web-app/pages/index.js
+++ workspaces/web-app/pages/index.js
@@ -1,18 +1,14 @@
 import React from 'react'
-import styled, { hydrate } from 'react-emotion'
+import styled from '@emotion/styled'
+import { css } from '@emotion/core'
 
-// Adds server generated styles to emotion cache.
-// '__NEXT_DATA__.ids' is set in '_document.js'
-/* istanbul ignore if */
-if (typeof window !== 'undefined' && window.__NEXT_DATA__) {
-  hydrate(window.__NEXT_DATA__.ids)
-}
+const headerStyles = css`
+  font-size: 20px;
+`
 
-const H1 = styled.h1(
-  {
-    fontSize: 20
-  },
-  props => ({ color: props.color })
-)
+const H1 = styled.h1`
+  ${headerStyles};
+  color: ${props => props.color};
+`
 
 export default () => <H1 color='#ff80c3'>Welcome to next.js!</H1>
