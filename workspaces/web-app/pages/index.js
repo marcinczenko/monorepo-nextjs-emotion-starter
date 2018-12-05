4a05bc910e1bee09c56ac704aad4a0c84ad4fa01
Jeroen Knoops <jeroen.knoops@gmail.com>
2018-12-05 15:16:31 +0100

    Replaces interpolated strings for objects

--- workspaces/web-app/pages/index.js
+++ workspaces/web-app/pages/index.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import styled from '@emotion/styled'
-import { css } from '@emotion/core'
 
-const headerStyles = css`
-  font-size: 20px;
-`
+const H1 = styled.h1(
+  {
+    fontSize: 20,
+  },
+  props => ({ color: props.color })
+)
 
-const H1 = styled.h1`
-  ${headerStyles};
-  color: ${props => props.color};
-`
-
-export default () => <H1 color='#ff80c3'>Welcome to next.js!</H1>
+export default () => <H1 color="#ff80c3">Welcome to next.js!</H1>
