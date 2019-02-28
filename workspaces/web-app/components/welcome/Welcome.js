0635595c777ac221c0996fa8da4441c736f7d364
Marcin Czenko <marcin@everydayproductive.com>
2019-02-28 23:54:25 +0100

    Adds click handler that alternates semantic-ui icon

--- workspaces/web-app/components/welcome/Welcome.js
+++ workspaces/web-app/components/welcome/Welcome.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { PageCentered, Centered, Spacer } from '@react-frontend-developer/react-layout-helpers'
 import { Segment, Button, Icon, Header } from 'semantic-ui-react'
 
 const Welcome = () => {
+  const [ iconName, setIconName ] = useState('thumbs up')
+
+  const onClick = () => {
+    iconName === 'thumbs up'
+      ? setIconName('thumbs up outline')
+      : setIconName('thumbs up')
+  }
+
   return (
     <PageCentered>
-      <Segment raised css={{ width: '80%', maxWidth: '500px' }}>
+      <Segment raised css={{ width: '80%', minHeight: '254px', maxWidth: '500px' }}>
         <Centered>
           <Header as='h1'>Welcome</Header>
-          <Icon name='thumbs up' size='massive' />
+          <Icon name={iconName} size='massive' />
           <Spacer margin='30px 0 0 0'>
-            <Button basic color='black'>Click Me!</Button>
+            <Button onClick={onClick} basic color='black'>Click Me!</Button>
           </Spacer>
         </Centered>
       </Segment>
